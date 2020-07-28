4bcac6aa14f23b7792be1096844e6db9cc8ced1a
Eliot Szwajkowski <eliotpszw@gmail.com>
2020-07-27 20:38:16 -0400

    Added building/testing to travis, fixed lint errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, './client/index.js'),
+  entry: path.join(__dirname, './client/index.jsx'),
   output: {
     filename: 'main.js',
     path: path.join(__dirname, './dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   devtool: 'source-map',
   mode: 'development',
